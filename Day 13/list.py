d24d206cdd2114488fa78a10781fb7c04b6dbd58
mani-bharathi-only <manibharathi0819@gmail.com>
2026-06-24 12:18:35 +0530

    Day 13 lambda fun updated

---
+++ Day 13/list.py
@@ -0,0 +1,21 @@
+list1='Python'
+print(list(list1))
+
+numbers=[i for i in range(11)]
+print(numbers)
+
+#Even Numbers
+numb=[i for i in range(10) if i%2==0]
+print(numb)
+
+#Lambda Function
+sum_num=lambda a,b:a+b
+print(sum_num(2,3))
+
+#Self Invoking Lambda
+print((lambda x,y,z:x**y+z)(2,3,4))
+
+#Lambda inside another function
+def num(x):
+    return lambda y:x**y
+print(num(2)(3))
